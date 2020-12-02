026afe74670bc79e8f5024b7b4ec11679b35b88e
jzwart <jzwart@usgs.gov>
2020-12-02 15:06:44 -0500

    updating temp fig

--- src/fig_temp_sens.R
+++ src/fig_temp_sens.R
@@ -19,7 +19,7 @@ library(scales)
 #
 # ggplot(metab_t, aes(x = temp, y = log(metab_t/temp), group = E, color = E)) + geom_line()
 r20 = 1
-temp = seq(5,30)
+temp = seq(5,31,by = .1)
 
 r<-r20*1.047^(temp-20)
 
@@ -30,6 +30,9 @@ q10 = c(1.6, 2.5)
 r = crossing(temp, r20, q10) %>%
   mutate(r = r20*q10^((temp - 15)/10))
 
+double_r = min(r$temp[r$r>=2&r$q10==2.5])
+double_gpp = min(r$temp[r$r>=2&r$q10==1.6])
+
 windows()
 plot_out = ggplot(r, aes(x = temp, y = r, group = as.character(q10), color = as.character(q10))) +
   geom_line(size = 2)+
@@ -39,7 +42,11 @@ plot_out = ggplot(r, aes(x = temp, y = r, group = as.character(q10), color = as.
   theme(axis.text = element_text(size = 16),
         axis.title = element_text(size = 20),
         legend.text = element_text(size = 16)) +
-  scale_color_manual(name ='', values = c('dark green', 'darkgoldenrod2'), labels=c('GPP', 'R'))
+  scale_color_manual(name ='', values = c('dark green', 'darkgoldenrod2'), labels=c('GPP', 'R')) +
+  geom_segment(aes(x = 0, y = 2, xend = double_gpp, yend = 2),color = 'black', linetype ='dashed' )+
+  geom_segment(aes(x = double_r, y = 0, xend = double_r, yend = 2),color = 'darkgoldenrod2', linetype ='dashed' )+
+  geom_segment(aes(x = double_gpp, y = 0, xend = double_gpp, yend = 2),color = 'dark green', linetype ='dashed' ) +
+  coord_cartesian(ylim = range(r$r), xlim = range(r$temp))
 
 plot_out
 
